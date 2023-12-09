d9fd1a89c018440fd74092096579aaab795ba805
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2023-12-08 20:32:48 -0800

    new working repo

---
+++ main/d1.go
@@ -0,0 +1,9 @@
+package main
+
+func d1p1() int {
+	return 1
+}
+
+func d1p2() int {
+	return 0
+}
---
+++ main/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Part 1: ", d1p1())
+	fmt.Println("Part 2: ", d1p2())
+}
