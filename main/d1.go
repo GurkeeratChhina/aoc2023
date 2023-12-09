49c59b4e17f9ec9176cf6c79e1fdb57824845d2a
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2023-12-08 21:46:47 -0800

    d1p1

--- main/d1.go
+++ main/d1.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+)
+
+const input = "../inputs/d1.txt"
+
 func d1p1() int {
-	return 1
+	sum := 0
+
+	file, err := os.Open(input)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		r, _ := regexp.Compile("[^\\d]")
+		digits := r.ReplaceAllString(scanner.Text(), "")
+		sum += 10*int(digits[0]-'0') + int(digits[len(digits)-1]-'0')
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return sum
 }
 
 func d1p2() int {
--- main/main.go
+++ main/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
 
 func main() {
 	fmt.Println("Part 1: ", d1p1())
