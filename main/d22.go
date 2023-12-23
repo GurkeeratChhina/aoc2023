1bf72a1a0af20b4113076e08b97372cdd08edb92
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2023-12-22 16:48:16 -0800

    d22p1 works, p2 pointer hell

---
+++ main/d22.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+const input22 = "../inputs/d22.txt"
+
+type tetris struct {
+	supporting   map[*tetris]bool
+	supported_by map[*tetris]bool
+	points       []*threepoint
+	lowest_z     int
+}
+
+type threepoint struct {
+	x, y, z int
+}
+
+type tetrisgrid map[int]map[int]map[int]*tetris
+
+func (tg tetrisgrid) addpoint(p *threepoint, data *tetris) {
+	_, ok := tg[p.x]
+	if !ok {
+		tg[p.x] = make(map[int]map[int]*tetris)
+		tg[p.x][p.y] = make(map[int]*tetris)
+		tg[p.x][p.y][p.z] = data
+		return
+	}
+	_, ok = tg[p.x][p.y]
+	if !ok {
+		tg[p.x][p.y] = make(map[int]*tetris)
+		tg[p.x][p.y][p.z] = data
+		return
+	}
+	_, ok = tg[p.x][p.y][p.z]
+	if ok {
+		panic("value already exists at key!")
+	}
+	tg[p.x][p.y][p.z] = data
+	return
+}
+
+func (t tetris) movedown(g tetrisgrid) {
+	found := false
+	for {
+		for _, p := range t.points {
+			if p.z-1 == 0 {
+				// reached bottom
+				for _, q := range t.points {
+					g.addpoint(q, &t)
+				}
+				return
+			}
+			// fix this line
+			below, ok := g[p.x][p.y][p.z-1]
+			if ok {
+				// found a tetris piece below
+				t.supported_by[below] = true
+				below.supporting[&t] = true
+				found = true
+			}
+		}
+		if found {
+			for _, q := range t.points {
+				g.addpoint(q, &t)
+			}
+			return
+		}
+		for _, q := range t.points {
+			q.z = q.z - 1
+		}
+	}
+}
+
+func (t tetris) is_removable() bool {
+	for block := range t.supporting {
+		if len(block.supported_by) == 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func (t *tetris) will_fall(removed map[*tetris]bool) bool {
+	for s := range t.supported_by {
+		if _, ok := removed[s]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (t *tetris) remove(removed map[*tetris]bool) {
+	removed[t] = true
+	for s := range t.supporting {
+		fmt.Println(t, "is supporting", s)
+		if s.will_fall(removed) {
+			s.remove(removed)
+		}
+	}
+}
+
+// sorting for tetris.lowest_z
+func sort_tetris_by_height(input []*tetris) (sorted []*tetris) {
+	length := len(input)
+	if length == 1 {
+		return input
+	}
+	lefthalf := sort_tetris_by_height(input[:length/2])
+	righthalf := sort_tetris_by_height(input[length/2:])
+	for i, j := 0, 0; ; {
+		if i == len(lefthalf) {
+			sorted = append(sorted, righthalf[j:]...)
+			return
+		} else if j == len(righthalf) {
+			sorted = append(sorted, lefthalf[i:]...)
+			return
+		} else if lefthalf[i].lowest_z <= righthalf[j].lowest_z {
+			sorted = append(sorted, lefthalf[i])
+			i++
+		} else {
+			sorted = append(sorted, righthalf[j])
+			j++
+		}
+	}
+}
+
+func make_tetris(input string) *tetris {
+	res := tetris{}
+	coords := extract_nums(input)
+	if coords[0] != coords[3] {
+		// x values are different
+		for i := coords[0]; i <= coords[3]; i++ {
+			res.points = append(res.points, &threepoint{i, coords[1], coords[2]})
+		}
+	} else if coords[1] != coords[4] {
+		// y values are different
+		for i := coords[1]; i <= coords[4]; i++ {
+			res.points = append(res.points, &threepoint{coords[0], i, coords[2]})
+		}
+	} else if coords[2] != coords[5] {
+		// z values are different
+		for i := coords[2]; i <= coords[5]; i++ {
+			res.points = append(res.points, &threepoint{coords[0], coords[1], i})
+		}
+	} else {
+		// single point block
+		res.points = append(res.points, &threepoint{coords[0], coords[1], coords[2]})
+	}
+	res.lowest_z = coords[2]
+	res.supported_by = make(map[*tetris]bool)
+	res.supporting = make(map[*tetris]bool)
+	return &res
+}
+
+func d22p1() int {
+	defer timeTrack(time.Now(), "d22p1")
+	f, err := os.Open(input22)
+	check(err)
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	var tetrislist []*tetris
+	for scanner.Scan() {
+		tetrislist = append(tetrislist, make_tetris(scanner.Text()))
+	}
+
+	tetrislist = sort_tetris_by_height(tetrislist)
+	grid := make(tetrisgrid)
+	for _, t := range tetrislist {
+		t.movedown(grid)
+	}
+
+	count := 0
+	for _, t := range tetrislist {
+		if t.is_removable() {
+			count++
+		}
+	}
+	return count
+}
+
+func d22p2() int {
+	defer timeTrack(time.Now(), "d22p2")
+	f, err := os.Open(inputtest)
+	check(err)
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	var tetrislist []*tetris
+	for scanner.Scan() {
+		tetrislist = append(tetrislist, make_tetris(scanner.Text()))
+	}
+
+	tetrislist = sort_tetris_by_height(tetrislist)
+	grid := make(tetrisgrid)
+	for _, t := range tetrislist {
+		t.movedown(grid)
+	}
+
+	sum := 0
+	for _, t := range tetrislist {
+		removed := make(map[*tetris]bool)
+		t.remove(removed)
+		fmt.Println(t, removed)
+		sum += len(removed) - 1
+	}
+
+	return sum
+}
--- main/main.go
+++ main/main.go
@@ -148,6 +148,6 @@ func timeTrack(start time.Time, name string) {
 }
 
 func main() {
-	fmt.Println("Part 1: ", d21p1())
-	fmt.Println("Part 2: ", d21p2())
+	fmt.Println("Part 1: ", d22p1())
+	fmt.Println("Part 2: ", d22p2())
 }
